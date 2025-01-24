49aa6596a29b6f570268d7863f1ec41b0425566e
Hadrien Levionnois <hlevionnois@hellowork.com>
2025-01-24 10:09:13 +0100

    fix: inject service in construct

--- src/main/java/api/adr/domain/fetcher/handlers/GetFetcherHandler.java
+++ src/main/java/api/adr/domain/fetcher/handlers/GetFetcherHandler.java
@@ -2,7 +2,6 @@ package api.adr.domain.fetcher.handlers;
 
 import api.adr.domain.fetcher.Fetchers;
 import api.adr.domain.fetcher.output.GetFetcherOutput;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -10,8 +9,11 @@ import java.util.Optional;
 @Component
 public class GetFetcherHandler {
 
-    @Autowired
     Fetchers fetchers;
+
+    GetFetcherHandler(Fetchers fetchers) {
+        this.fetchers = fetchers;
+    }
 
     public Optional<GetFetcherOutput> invoke(long id) {
         return fetchers.findById(id).map(
